0a8ee1459e2dad9ce4fe3d76dba311da26e7b180
François Parmentier <francois.parmentier@gmail.com>
2021-01-31 16:30:16 +0000

    Euler's test

--- src/Euler.purs
+++ src/Euler.purs
@@ -1,11 +1,14 @@
 module Euler where
 
 import Prelude
-import Data.List (range, filter)
+import Data.List (range, filter, List)
 import Data.Foldable (sum)
 
+ns :: List Int
 ns = range 0 999
 
+multiples :: List Int
 multiples = filter (\n -> mod n 3 == 0 || mod n 5 == 0) ns
 
+answer :: Int
 answer = sum multiples
