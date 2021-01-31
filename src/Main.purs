6a01b22535cd88b8dddf9871688960ab15eff26e
François Parmentier <francois.parmentier@gmail.com>
2021-01-31 16:47:41 +0000

    Main use Euler

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,10 @@
 module Main where
 
 import Prelude
-
+import Euler (answer)
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log ("The answer is " <> show answer)
